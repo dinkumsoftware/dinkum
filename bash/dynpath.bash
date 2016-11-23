efa7878fd0b00d89b7a749e29d2027217cdac275
tom campbell <tom@rail-pod.com>
2016-11-22 19:35:30 -0500

    bash dynpath bug fix.  Properly replace ~
    Also replaced ... with -, but that code is buggy.

--- bash/dynpath.bash
+++ bash/dynpath.bash
@@ -2,13 +2,20 @@
 # /usr/share/doc/foo would become /u…/s…/d…/foo
 # Usage: PS1="\u@\h \$(_dynpath) \$ "
 
+# Initial from https://gist.github.com/ehamberg/1197104
+# 2016-11-22 tc@DinkumSoftware Bug fixes:
+#    Wasn't inserting ~
+#    eclipse char (...) wasn't printing.  Replaced with -
+
 _dynpath()
 {
-  CURR="${PWD/#$HOME/~}"
+  # Get current directory, replacing /home/username with ~
+  CURR="${PWD/#$HOME/'~'}"
   MAX_WIDTH=$(($COLUMNS/4))
 
-  while [[ ${#CURR} -gt $MAX_WIDTH && $CURR =~ /.[^…].*/ ]] ; do
-    CURR=$(echo ${CURR}|sed 's,/\(\.\?.\)[^…/]\+/,/\1…/,')
+  while [[ ${#CURR} -gt $MAX_WIDTH && $CURR =~ /.[^-].*/ ]] ; do
+    CURR=$(echo ${CURR}|sed 's,/\(\.\?.\)[^-/]\+/,/\1-/,')
+    echo "debug:$CURR"
   done
   echo $CURR
 }
