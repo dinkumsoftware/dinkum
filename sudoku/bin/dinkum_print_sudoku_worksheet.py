79ae5720878d815ae0de0166ce38da711cb07ad6
tom campbell <tc@DinkumSoftware.com>
2019-11-07 16:42:23 -0500

    sudoku working commit. Got packages to work by diddling .bashrc. sudoku.py test code fails.  dinkum_print_sudoku_worksheet.py in progress.

--- sudoku/bin/dinkum_print_sudoku_worksheet.py
+++ sudoku/bin/dinkum_print_sudoku_worksheet.py
@@ -4,6 +4,7 @@
 labeled with number.  An example is shown below:
 
 <todo> replace this with actual example
+<todo> replace ALL examples with real examples
 Ignore next two lines, just ruler used to write the code
 0         1         2         3
 -123456789-123456789-123456789-123456789
@@ -44,50 +45,63 @@ from dinkum.sudoku.sudoku import Board
 horz_line_char = '-'
 vert_line_char = '|'
 
-cell_width   = 4  # width of printed cell.  Includes rightmost | but not leftmost |
-left_width_to_first_cell = 2
-right_width_after_last_cell = 1
-output_width = left_width_to_first_cell + right_width_after_last_cell + \
-               cell_width * Board.rcb_size          # line length excluding \n
+# size of printed cell
+# includes rightmost | and topmost -
+# does NOT include leftmost | and bottom -
+cell_width   = 6
+cell_height  = 6
 
+# Width of whole printed board
+left_offset_to_first_cell = 2     # Accounts for row label and one |
+right_pad_after_last_cell = 3     # last cell | + | + row_label
+output_width = left_offset_to_first_cell + cell_width * Board.rcb_size + right_pad_after_last_cell
+
+# Height of whole printed board
+top_offset_to_first_cell = 2 # column label + '-'
+bot_pad_after_last_cell = 3  # bottom cell '-' + '-' + column_label
 
 def top_or_bottom_lines(is_top) :
     ''' returns [] of [column label line, horz separator line. e.g.
     <todo> example here
     is_top controls whether top or bottom lines
     '''
-    hsl = horz_separator_lines(is_top) # ------
-    cll = col_label_lines()            # 0 1 2 ...
 
-    # The order in the list depends on whether to or bottom
-    return cll+hsl if is_top else hsl+cll
+    if is_top :
+        # column label and separator line
+        ret_list  = col_label_lines()               # 0 1 2 ...
+        ret_list += horz_separator_lines('/', '\\') # /----------\
+        assert len(ret_list) == top_offset_to_first_cell
+        return ret_list
 
-def horz_separator_lines(is_top) :
+    else :
+        # bottom
+        ret_list  = horz_separator_lines(vert_line_char, vert_line_char)  # |---------------|
+        ret_list += horz_separator_lines('\\', '/')                       # \---------------/ 
+        ret_list += col_label_lines()                                     #     0     1   2 ...
+
+        assert len(ret_list) == bot_pad_after_last_cell
+        return ret_list
+
+    assert False, "Impossible Place"
+
+
+def horz_separator_lines(first_char, last_char) :
     ''' Returns [] of lines making up top or bottom
     lines, e.g.
         <todo> put example here
-    is_top controls whether top or bottom lines
-    are returned.
-    les are NOT new line terminated.
+    The first and last chars of each line are set to
+    first_char and last_char
+    lines are NOT new line terminated.
     '''
-
-    # Assume it's the top line
-    first_char = '/'
-    last_char  = '\\'  # \\ ==> \ i.e. Only one char
-    if not is_top :
-        # Swap them
-        (last_char, first_char) = (first_char, last_char)
 
     # -----------
     # Start with full line of lines
     # and overwrite what we need to
     sep_line = horz_line_char * output_width
 
-    # ' /------------------'
-    sep_line = ' ' + first_char + sep_line[2:]
-
-    # ' /------------------\\'
-    sep_line = sep_line[:-1] + last_char
+    # overwrite first and last chars that were passed in                    
+    sep_line = replace_substr_at(sep_line, first_char,  0)
+    sep_line = replace_substr_at(sep_line, last_char , -1)
 
     # All done
     return [sep_line]
@@ -101,38 +115,49 @@ def col_label_lines() :
     col_label_line = ' ' * output_width
 
     for col_num in range(Board.rcb_size) :
-        # Replace the space at left_width_to_first_cell + cell_width + cell_width * col_num
+        # Replace the space at left_offset_to_first_cell + half of cell_width + cell_width * col_num
         # with the column number.
-        # Extra cell_width above right justifies the column_label in cell
+        # Extra half cell_width above centers justifies the column_label in cell
+        offset = left_offset_to_first_cell + cell_width//2 + col_num * cell_width
 
-        offset = left_width_to_first_cell + cell_width + col_num * cell_width
-        col_label_line = col_label_line[:offset] + str(col_num) + col_label_line[offset:]
+        col_label_line = replace_substr_at(col_label_line, str(col_num), offset)
 
     return [col_label_line]
     
         
 def row_line(row_num) :
-    ''' returns [] of lines that make up a row.
+    ''' returns [] of lines that make up a row of cells
     lines are NOT \n terminated.  e.g.
+    <todo> replace
       1|  9 10 11 | 12 13 14 | 15 16 17 |1
+
+    The top line and leftmost line each cell is included in
+    returned lines.  The bottom and rightmost lines are NOT
+    included in returned lines.
     '''
 
-    # 1|
-    row_line = str(row_num) + vert_line_char
+    # What we return.  Gets filled in below                     
+    ret_lines= [None] * cell_height
 
-    # Do the cells (without vertical separator
-    # 0  1  2 |  3  4  5 |  6  7  8 |
-    cell_num = row_num * Board.rcb_size # cell_num of leftmost cell
-    for cell_in_line in range(Board.rcb_size) :
-        cell_line = "%3s " % str(cell_num)
-        cell_num += 1
+    # Start with a separator line
+    ret_lines[0] = horz_separator_lines(vert_line_char, vert_line_char)
 
-        row_line += cell_line
+    for line_num_in_row in range(1, cell_height) :
+        line = ' ' * output_width # start with line of spaces
+        line = replace_substr_at(line,  1, vert_line_char)  # outside left vert line
+        line = replace_substr_at(line, -2, vert_line_char)  # outside right vert line
+                     
+        # Time to place row label on outside ?
+        if line_num_in_row == cell_height//2 :
+            line = replace_substr_at(line,  0, str(row_num))  # outside left vert line
+            line = replace_substr_at(line, -1, str(row_num))  # outside right vert line
+            
+        # All done composing line
+        assert len(line) == output_width
 
-    # Label row on right
-    row_line += str(row_num)
-
-    return [row_line]
+        # Set our result in the [] we return
+        ret_lines[line_num_in_row] = line
+        assert len(ret_lines) == cell_height
 
 def worksheet() :
     ''' creates a blank sudoku worksheet, i.e.
@@ -153,14 +178,56 @@ def worksheet() :
 
     # Stuff in the middle, e.g.
     # 1|  9 10 11 | 12 13 14 | 15 16 17 |1
-    for row_num in range(Board.rcb_size) :
-        ws += row_line(row_num)
+################################################
+#    for row_num in range(Board.rcb_size) :
+#        ws += row_line(row_num)
 
     # \ --------------------------------/ #
     #    0  1  2 ...
     ws += top_or_bottom_lines(is_top=False)
         
     return ws
+
+def replace_substr_at(s, substr, offset_into_s, num_substr_chars=1) :
+    ''' Replaces num_substr_chars at s[offset_into_s] with
+    first num_substr_chars in substr and returns the new s
+
+    if offset_into_s is negative, counts from end of s, a la 
+    slice (:).  i.e. -1 refers to last char of s
+
+    offset_into_s and num_substr_chars are clipped to insure
+    that they refer to valid chars in both s and substr.
+
+    The length of s will never change.  The number of
+    chars extracted from substr always equal the number
+    of chars in s that are returned
+    '''
+    original_s_length = len(s) 
+
+    # Handle offsets from end of string
+    # Clip to first char on big negative offsets
+    if offset_into_s < 0 :
+        offset_into_s = len(s) + offset_into_s
+        if offset_into_s < 0 :
+            offset_into_s = 0  # clip
+
+    # clip to make sure offset_in_s and num_substr_chars
+    # refer to chars in both s and substr, i.e. not off the end
+    min_length = min(len(s), len(substr) )
+    if offset_into_s + num_substr_chars > min_length :
+        num_substr_char = min_length - offset_into_s
+
+    # Do the replacement
+    # example: s:abcdefg substr:123 offset_into_s:3 num_substr_chars:1
+    leading_s   = s[:offset_into_s]                   # abc
+    replacement = substr[:num_substr_chars]           # 1
+    trailing_s  = s[offset_into_s+num_substr_chars:]  # efg
+
+    s = leading_s + replacement + trailing_s          #abc1efg
+
+    assert len(s) == original_s_length
+    return s 
+
 
 if __name__ == "__main__" :
 
