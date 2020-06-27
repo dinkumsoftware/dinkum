034bec23f8755b41e509fcdff6efa7b72c6eab79
tom campbell <tc@DinkumSoftware.com>
2020-06-27 09:30:58 -0400

    emacs init diddle

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,11 @@
 ;; Read tc of Dinkum Software's emacs customization
 ;; See https://github.com/dinkumsoftware/dinkum.git/emacs
 ;; written by dinkum-install-tc-emacs on Thu Apr 11 15:50:32 EDT 2019
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (load "~/projects/dinkum/emacs/tc-init.emacs")
