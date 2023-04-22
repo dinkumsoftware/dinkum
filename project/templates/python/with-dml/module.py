aa8a2f2c9b4f1ac82d641d0360641241905632ba
tom campbell <tc@DinkumSoftware.com>
2023-04-22 11:18:19 -0400

    remove dml from templates

---
+++ project/templates/python/with-dml/module.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+#<filename> [filename] <fixme>
+#<path>  [path] <fixme>
+#<repo> https://github.com/dinkumsoftware/dinkum.git
+
+#<mod_doc>
+'''
+    <fixme>
+'''
+#<\mod_doc>
+
+#<copyright> Copyright (c) 2020 Dinkum Software
+#<lic>
+'''Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+''' #</lic>
+
+#<authors> tc    tom campbell, www.DinkumSoftware.com <todo> change to .org
+
+#<history>
+#    2020-10-02 tc Initial
+#</history>
+
+
+#<unittests>
+import unittest
+
+#<fixme>
+class Test_MODULE_NAME_HERE(unittest.TestCase):
+        
+    #<fixme>
+    def test_FUNCTION_NAME_HERE(self) :
+        ''' <fixme> '''
+        pass # <fixme>
+        
+
+if __name__ == "__main__" :
+    # Run the unittests
+    unittest.main()
+    
+
+
+
+#</unittests>
